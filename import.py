d59279aec2c79cff74ef576915052739d75e740b
inghamn <inghamn@bloomington.in.gov>
2018-04-19 16:51:52 -0400

    Woking version with publically shared spreadsheets

--- import.py
+++ import.py
@@ -1,19 +1,28 @@
-import yaml
+import errno
+import os
 import pycurl
+import yaml
 
 download = pycurl.Curl();
 upload   = pycurl.Curl();
 config   = open('./config.yml', 'r')
 docs     = yaml.load_all(config)
 
+download.setopt(pycurl.FOLLOWLOCATION, True)
+
 for doc in docs:
     tmp_dir  = doc['tmp_dir' ]
     log_file = doc['log_file']
-    ckan_url = doc['ckan']['url'] + '/api/3/action/resource_update'
-    api_key  = doc['ckan']['api']
+    ckan_url = doc['ckan']['url'    ] + '/api/3/action/resource_update'
+    api_key  = doc['ckan']['api_key']
 
     upload.setopt(pycurl.URL, ckan_url)
     upload.setopt(pycurl.HTTPHEADER, ['Authorization: ' + api_key])
+
+    try:
+        os.makedirs(tmp_dir, 0775)
+    except OSError as e:
+        if e.errno != errno.EEXIST: raise
 
     for s in doc['spreadsheets']:
         resource_id    = s['resource_id'   ]
@@ -34,5 +43,6 @@ for doc in docs:
                     pycurl.FORM_CONTENTTYPE, 'text/csv'
                 ))
             ])
+            upload.perform()
 download.close()
 upload.close()
